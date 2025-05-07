0a8b79e82e2dfd43e87b88509d8118bc04476e2b
Chandrasekhar174 <117912184+Chandrasekhar174@users.noreply.github.com>
2025-05-07 07:57:44 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
 const mysql=require('mysql2');
 const connection=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'Ratnakar@8342',
-    database:'todo_app'
+    host:' sql12.freesqldatabase.com',
+    user:'sql12777368',
+    password:'l8Yd7MpyZj',
+    database:'sql12777368'
 });
 
 connection.connect((err)=>{
