5e07e8e270e9b74942a40fca9935dbff90af7a9b
Chandrasekhar174 <117912184+Chandrasekhar174@users.noreply.github.com>
2025-05-16 13:17:24 +0530

    Update api.jsx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API=axios.create({
-    baseURL:'https://fullstacktodoappication-1.onrender.com/api',
+    baseURL:'https://todoapp-62b4.onrender.com/api',
 });
 
 API.interceptors.request.use((req)=>{
